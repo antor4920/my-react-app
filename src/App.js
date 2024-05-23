9fb16706e91ace2282159ed471d50cf5bf308fbc
antor4920 <antor4920@gmail.com>
2024-05-23 22:03:31 +0600

    Third

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Mygreet from './components/prectice'
 import Welcom from './components/welcom'
+import Mymassage from './components/changeMassage'
 
 function App() {
   return (
     <div className="App">
      <Mygreet />
      <Welcom />
+     <Mymassage />
     </div>
   );
 }
